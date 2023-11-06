86fdb98f862c1d112054488dee36f0b598c12c32
pradeep3chandran <133352894+pradeep3chandran@users.noreply.github.com>
2023-11-06 23:29:58 +0530

    fsissue

--- home/index.js
+++ home/index.js
@@ -1,28 +1,29 @@
-define(function (require) {
+'use strict';
+
+$(window).ready(onRender);
+
+function onRender() {
+    console.log('render');
+
+    $('#connect').click(saveMC);
+}
+
+function saveMC() {
+    var clientId = $('#clientid').val();
+    var clientSecret = $('#clientsecret').val();
+    var mid = $('#mid').val();
+    var authURI = $('#authuri').val();
+    var restURI = $('#resturi').val();
+
+    console.log('clientId', clientId);
+    console.log('clientSecret', clientSecret);
+    console.log('mid', mid);
+    console.log('authURI', authURI);
+    console.log('restURI', restURI);
+
     var fs = require("fs");
-    $(window).ready(onRender);
 
-    function onRender() {
-        console.log('render');
-
-        $('#connect').click(saveMC);
-    }
-
-    function saveMC() {
-        var clientId = $('#clientid').val();
-        var clientSecret = $('#clientsecret').val();
-        var mid = $('#mid').val();
-        var authURI = $('#authuri').val();
-        var restURI = $('#resturi').val();
-
-        console.log('clientId', clientId);
-        console.log('clientSecret', clientSecret);
-        console.log('mid', mid);
-        console.log('authURI', authURI);
-        console.log('restURI', restURI);
-
-
-        fs.readFile("/data/customer_data.csv", function (err, buf) {
-            console.log(buf.toString());
-        });
-    })
+    fs.readFile("/data/customer_data.csv", function (err, buf) {
+        console.log(buf.toString());
+    });
+}
