82da9bc31c5170d44c6fbfa5cdafd82cc44697c8
pradeep3chandran <133352894+pradeep3chandran@users.noreply.github.com>
2023-11-06 21:47:58 +0530

    reqq

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.get('/js/index.js', function (req, res) {
     return res.sendFile(`${options.rootDirectory}/js/index.js`);
 });
 
+app.get('/js/require.js', function (req, res) {
+    // Journey Builder looks for config.json when the canvas loads.
+    // We'll dynamically generate the config object with a function
+    return res.sendFile(`${options.rootDirectory}/js/require.js`);
+});
+
 submodules.forEach((sm) => sm(app, {
     rootDirectory: __dirname,
 }));
