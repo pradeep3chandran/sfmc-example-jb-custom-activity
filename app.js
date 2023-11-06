053361c41143278b9040eebd5f66acc358e50f4a
pradeep3chandran <133352894+pradeep3chandran@users.noreply.github.com>
2023-11-06 21:52:08 +0530

    js

--- app.js
+++ app.js
@@ -18,19 +18,19 @@ app.use('/', express.static(path.join(__dirname, 'home')));
 app.use('/assets', express.static(path.join(__dirname, '/node_modules/@salesforce-ux/design-system/assets')));
 
 
-app.get('/js/jquery.min.js', function (req, res) {
+app.get('/jquery.min.js', function (req, res) {
     // Journey Builder looks for config.json when the canvas loads.
     // We'll dynamically generate the config object with a function
     return res.sendFile(`${options.rootDirectory}/js/jquery.min.js`);
 });
 
-app.get('/js/index.js', function (req, res) {
+app.get('/index.js', function (req, res) {
     // Journey Builder looks for config.json when the canvas loads.
     // We'll dynamically generate the config object with a function
     return res.sendFile(`${options.rootDirectory}/js/index.js`);
 });
 
-app.get('/js/require.js', function (req, res) {
+app.get('/require.js', function (req, res) {
     // Journey Builder looks for config.json when the canvas loads.
     // We'll dynamically generate the config object with a function
     return res.sendFile(`${options.rootDirectory}/js/require.js`);
