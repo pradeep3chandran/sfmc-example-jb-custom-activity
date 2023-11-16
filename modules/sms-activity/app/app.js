0abca76358ec95fed4db2e514e9af03bfa78a9fe
pradeep3chandran <133352894+pradeep3chandran@users.noreply.github.com>
2023-11-16 20:26:26 +0530

    tyu

--- modules/sms-activity/app/app.js
+++ modules/sms-activity/app/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const configJSON = require('../config/config-json');
 const bodyParser = require('body-parser');
-const activityUtils = require('../src/activityUtil');
+const fs = require('fs');
+const path = require('path');
+const csv = require("fast-csv");
 
 const errorObject = {
     52992: "Username / Password incorrect",
@@ -148,11 +150,198 @@ module.exports = function smsActivityApp(app, options) {
 
 
 
-    app.post('/modules/sms-activity/execute', async function (req, res) {
+    app.post('/modules/sms-activity/execute', function (req, res) {
         console.log('debug: /modules/sms-activity/execute');
         console.log('req ', req);
+        let fileData = [];
 
         const request = req.body;
+
+        console.log('request ', request);
+        // Find the in argument
+        function getInArgument(k) {
+            if (request && request.inArguments) {
+                for (let i = 0; i < request.inArguments.length; i++) {
+                    let e = request.inArguments[i];
+                    if (k in e) {
+                        return e[k];
+                    }
+                }
+            }
+        }
+
+
+        const mobileNumber = getInArgument('toNumber') || 'nothing';
+        const senderName = getInArgument('senderName') || 'nothing';
+        const mid = getInArgument('mid') || 'nothing';
+        const message = getInArgument('message') || 'nothing';
+        const primaryKey = getInArgument('primaryKey') || 'nothing';
+        const campaignName = getInArgument('campaignName') || 'nothing';
+
+        const jsonStr = {
+
+            "@VER": "1.2",
+
+            "USER": {},
+
+            "DLR": {
+
+                "@URL": "https://marketing-configuration-app-6564d07cc826.herokuapp.com/modules/sms-activity/deliveryreport?TO=%p&MSG_STATUS=%16&CLIENT_GUID=%5&STATUS_ERROR=%4&DELIVERED_DATE=%3&TEXT_STATUS=%13&MESSAGE_ID=%7&TAG=%TAG&CLIENT_SEQ_NUMBER=%6&REASON_CODE=%2"
+
+            },
+
+            "SMS": [
+
+                {
+
+                    "@UDH": "0",
+
+                    "@CODING": "1",
+
+                    "@TEXT": message,
+
+                    "@PROPERTY": "0",
+
+                    "@ID": primaryKey,
+
+                    "ADDRESS": [
+
+                        {
+
+                            "@FROM": senderName,
+
+                            "@TO": mobileNumber,
+
+                            "@SEQ": "12",
+
+                            "@TAG": "db1"
+
+                        }
+
+                    ]
+
+                }
+
+            ]
+
+        }
+
+        console.log('jsonStr: ', JSON.stringify(jsonStr));
+
+        fs.createReadStream(path.join('./data/customer_data.csv'))
+            .pipe(csv.parse({ headers: true }))
+            .on('error', error => console.error('err ', error))
+            .on('data', row => { if (mid == row.MID) { fileData.push(row) } })
+            .on('end', () => {
+                console.log('dataa ', fileData);
+
+
+
+                fetch('https://api.myvfirst.com/psms/api/messages/token?action=generate', {
+                    method: 'POST', headers: {
+                        "Authorization": 'Basic ' + Buffer.from(fileData[0].Username + ':' + fileData[0].Password).toString('base64')
+                    }
+                }).then(response => {
+                    console.log(response);
+
+                    response.json().then(data => {
+
+                        console.log('data ', data.token);
+                        let token = data.token;
+
+
+                        fetch('https://api.myvfirst.com/psms/servlet/psms.JsonEservice', {
+                            method: 'POST', headers: { "Authorization": 'Bearer ' + token, "Content-Type": 'application/json' }, body: JSON.stringify(jsonStr)
+                        }).then(response1 => {
+                            console.log(response1);
+
+                            response1.json().then(data1 => {
+                                //return res.status(200).json(data1);
+                                let reqBody = [];
+                                if (data1.MESSAGEACK.GUID) {
+                                    reqBody.push({
+                                        "keys": {
+                                            "GUID": data1.MESSAGEACK.GUID.GUID
+                                        },
+                                        "values": {
+                                            ID: data1.MESSAGEACK.GUID.ID,
+                                            SUBMIT_DATE: data1.MESSAGEACK.GUID.SUBMITDATE,
+                                            FROM: senderName,
+                                            TO: mobileNumber,
+                                            TEXT: message,
+                                            STATUS: 'Submitted',
+                                            CAMPAIGN_NAME: campaignName
+                                        }
+                                    });
+                                    if (data1.MESSAGEACK.GUID.ERROR) {
+                                        console.log('error');
+                                        reqBody[0].values.STATUS = 'Failed';
+                                        reqBody[0].values.ERROR_CODE = data1.MESSAGEACK.GUID.ERROR.CODE;
+                                        reqBody[0].values.ERROR_REASON = errorObject[data1.MESSAGEACK.GUID.ERROR.CODE];
+                                    }
+                                } else {
+                                    const date = new Date().toLocaleString();
+                                    reqBody.push({
+                                        "keys": {
+                                            "GUID": primaryKey + date
+                                        },
+                                        "values": {
+                                            ID: primaryKey,
+                                            SUBMIT_DATE: date,
+                                            FROM: senderName,
+                                            TO: mobileNumber,
+                                            TEXT: message,
+                                            STATUS: 'Failed',
+                                            ERROR_CODE: data1.MESSAGEACK.Err.Code,
+                                            ERROR_REASON: errorObject[data1.MESSAGEACK.Err.Code],
+                                            CAMPAIGN_NAME: campaignName
+                                        }
+                                    });
+                                }
+                                console.log(reqBody);
+
+                                let accessRequest = {
+                                    "grant_type": "client_credentials",
+                                    "client_id": fileData[0].Client_ID,
+                                    "client_secret": fileData[0].Client_Secret,
+                                    "account_id": fileData[0].MID
+                                };
+
+                                fetch(fileData[0].Auth_URI + '/v2/token', {
+                                    method: 'POST', body: JSON.stringify(accessRequest), headers: { 'Content-Type': 'application/json' }
+                                }).then(response2 => {
+
+                                    response2.json().then(data2 => {
+                                        console.log(data2);
+                                        console.log(reqBody);
+                                        fetch(fileData[0].Rest_URI + '/hub/v1/dataevents/key:CA054127-E2A5-494F-83EF-230B180A0F8E/rowset', {
+                                            method: 'POST', body: JSON.stringify(reqBody), headers: { 'Authorization': 'Bearer ' + data2.access_token, 'Content-Type': 'application/json' }
+                                        }).then(response3 => {
+
+                                            response3.json().then(data3 => {
+                                                console.log(data3);
+
+                                                return res.status(200).json('Success');
+                                            })
+                                        }).catch(err1 => {
+                                            return res.status(400).json(err);
+                                        });
+                                        //return res.status(200).json(data1);
+                                    })
+                                }).catch(err => {
+                                    return res.status(400).json(err);
+                                });
+                            })
+                        }).catch(err => {
+                            return res.status(400).json(err);
+                        });
+                    })
+                }).catch(err => {
+                    return res.status(400).json(err);
+                });
+            });
+
+        /*const request = req.body;
 
         console.log('request ', request);
         // Find the in argument
@@ -297,7 +486,7 @@ module.exports = function smsActivityApp(app, options) {
             })
         }).catch(err => {
             return res.status(400).json(err);
-        });
+        });*/
 
     });
 
