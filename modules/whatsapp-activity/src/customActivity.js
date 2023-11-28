41c3217423be04bfa66cd84a59ea45a49ce0bae9
pradeep3chandran <133352894+pradeep3chandran@users.noreply.github.com>
2023-11-28 16:55:33 +0530

    w

--- modules/whatsapp-activity/src/customActivity.js
+++ modules/whatsapp-activity/src/customActivity.js
@@ -355,6 +355,9 @@ define([
 
         if (bodyFields) {
             for (let obj of bodyFields) {
+                console.log(obj);
+                console.log(obj.val());
+                console.log(obj.Id);
                 console.log(obj.find('option:selected').attr('value'));
             }
         }
